5bfb02809dccec7ec5683e8b7d1d4153521db46d
ewohnlich <wohnlice@imsweb.com>
2026-06-17 17:20:34 -0400

    more github workflow

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ from importlib.metadata import version as meta_version
 project = "ims.sso"
 author = "Eric Wohnlich and Taina Dumay"
 
-extensions = ["sphinx.ext.autodoc", "sphinx_tabs.tabs", "sphinxarg.ext"]
+extensions = ["sphinx.ext.autodoc", "sphinx_tabs.tabs"]
 
 source_suffix = {
     ".rst": "restructuredtext",
