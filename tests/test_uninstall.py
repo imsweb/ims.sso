828049313cbb9488543af24f8f1f54584d02e1b1
ewohnlich <wohnlice@imsweb.com>
2026-06-17 17:44:17 -0400

    npm build

--- tests/test_uninstall.py
+++ tests/test_uninstall.py
@@ -1,8 +1,7 @@
 import pytest
+from ims.sso.interfaces import IBrowserLayer
 
-from ims.users.interfaces import IBrowserLayer
-
-PACKAGE_NAME = "ims.users"
+PACKAGE_NAME = "ims.sso"
 
 
 class TestUninstall:
@@ -11,7 +10,7 @@ class TestUninstall:
         installer.uninstall_product(PACKAGE_NAME)
 
     def test_product_uninstalled(self, installer):
-        """Test if ims.users is cleanly uninstalled."""
+        """Test if ims.sso is cleanly uninstalled."""
         assert not installer.is_product_installed(PACKAGE_NAME)
 
     def test_browserlayer_removed(self, browser_layers):
