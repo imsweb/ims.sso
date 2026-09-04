7d65dc86f53ba78839f1cdfc4dee0113ade62b91
Dumay, Taina (IMS) <dumayt@imsweb.com>
2026-09-04 14:55:10 -0400

    update

--- tests/test_active_status.py
+++ tests/test_active_status.py
@@ -44,24 +44,6 @@ class TestActivation:
         view.manageUser(users=(rec,))
         assert api.user.get("siteadmin").getProperty("active") == "inactive"
 
-    def test_cant_deactivate_manager(self, portal):
-        """User cannot deactivate someone with the Manager role"""
-        view = api.content.get_view("usergroup-userprefs", context=portal)
-        view.request.environ["HTTP_X_CSRF_TOKEN"] = createToken()
-        view.request.method = "POST"
-        rec = RequestRecord(id="manager", reset_email="noreply@nohost.com", active="inactive")
-        view.manageUser(users=(rec,))
-        assert api.user.get("manager").getProperty("active") == "active"
-
-    def test_cant_deactivate_service_account(self, portal):
-        """User cannot deactivate a service account"""
-        view = api.content.get_view("usergroup-userprefs", context=portal)
-        view.request.environ["HTTP_X_CSRF_TOKEN"] = createToken()
-        view.request.method = "POST"
-        rec = RequestRecord(id="service", reset_email="noreply@nohost.com", active="inactive")
-        view.manageUser(users=(rec,))
-        assert api.user.get("service").getProperty("active") == "active"
-
     def test_cant_deactivate_without_permission(self, portal):
         """User must have permission to deactivate/reactivate"""
         with api.env.adopt_user("siteadmin"):
